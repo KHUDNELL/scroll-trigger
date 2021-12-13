a4fc814321bf424c6f1824ab697e777850a02c5a
Ken Hudnell <khudnell252@gmail.com>
2021-12-13 17:03:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {gsap} from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+import { useEffect, useRef } from 'react';
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+
+  const titleRef = useRef();
+
+  useEffect(() => {
+    gsap.from(titleRef.current, {
+      y: 500,
+      scrollTrigger: {
+        trigger: titleRef.current
+      }
+
+    })
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="space"></div>
+      <h1 className="title" ref={titleRef}>Hello</h1>
+      <div className="space"></div>
     </div>
   );
 }
